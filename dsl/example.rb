222d0b7fb6659bc018bd7920aa41254c523ffc92
bl1ndy <ayoblindstone@gmail.com>
2022-11-23 14:57:40 +0300

    Add example

---
+++ dsl/example.rb
@@ -0,0 +1,50 @@
+class Foo
+  extend Attributes
+
+  define_attributes do
+    name do
+      required!
+    end
+
+    state do
+      enum %i[pending running stopped failed]
+      default :pending
+    end
+
+    started_at { default { Time.now } }
+
+    count do
+      default 0
+
+      actions do
+        incr! do
+          @count += 1
+        end
+        
+        decr! do
+          @count -= 1
+        end
+      end
+    end
+  end
+end
+
+# При отсутствии атрибута, помеченного как required! выдавать ошибку ArgumentError:
+# Foo.new(state: :running) => ArgumentError
+ 
+# Действия, перечисленные в actions должны быть преобразованы в методы объекта:
+# f.incr!
+# f.count => 1
+# f.decr!
+# f.count => 0
+ 
+# default может принимать статичное значение или блок, выполняющийся при инициализации объекта
+# f = Foo.new(name: 'test')
+# f.started_at => 2022-12-01 13:43:18 +0300
+ 
+# enum создает методы проверки текущего состояния и его изменения
+# f.state => :pending
+# f.pending? => true
+# f.running? => false
+# f.running!
+# f.running? => true
